2c71fc5632ab0985c7b5263b105cd9af476186c1
cmdallas <self@chrisdallas.tech>
2017-11-22 22:16:34 -0800

    Add monitoring for monitoring nodes, Cloudwatch integration

--- bin/monitor_monitoring_nodes.rb
+++ bin/monitor_monitoring_nodes.rb
@@ -2,24 +2,30 @@
 
 require_relative '../lib/aws_helper'
 
-def put_cloudwatch_data(namespace, name_metric, dimension_name, dimension_value, datapoint_value)
-# make PutMetricData api call to cloudwatch using custom metrics
-  @cloudwatch.put_metric_data({
-    namespace: namespace,
-    metric_data: [
-      {
-        metric_name: name_metric,
-        dimensions: [
-          {
-            name: dimension_name,
-            value: dimension_value,
-          },
-        ],
-        timestamp: Time.now,
-        value: datapoint_value,
-        unit: "Count",
-        storage_resolution: 1,
-      },
-    ],
-  })
+def disk_check
+  disk_used = `df -h | awk '$NF=="/"{printf "%s", $5}'`
+  disk_used = (disk_used.gsub('%', '')).to_i
+  put_cloudwatch_data('MonitorServers', 'DiskUsed', 'Monitoring Host Metrics', ip_fetcher, disk_used)
+end
+
+def memory_check
+# memory used as a percentage
+  memory_used = `free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }'`
+  memory_used = memory_used.to_i
+  put_cloudwatch_data('MonitorServers', 'MemoryUsed', 'Monitoring Host Metrics', ip_fetcher, disk_used)
+end
+
+def ip_fetcher
+  server_ip = `/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+  server_ip = (server_ip.gsub("\n", '')).to_s
+end
+
+def main
+  cloudwatch_client_constructor
+  disk_check
+  memory_check
+end
+
+if __FILE__ == $0
+  main
 end
--- lib/aws_helper.rb
+++ lib/aws_helper.rb
@@ -1,14 +1,42 @@
 #!/user/bin/env ruby
 
 require 'aws-sdk-cloudwatch'
+require 'aws-sdk-ec2'
 require 'aws-sdk-sns'
 
+# CloudWatch
 def cloudwatch_client_constructor
   credentials = Aws::SharedCredentials.new(profile_name: 'default')
-  Aws::EC2::Client.new(credentials: credentials)
+  Aws::EC2::Client.new(
+    credentials: credentials,
+    region: 'us-west-2'
+  )
   @cloudwatch = Aws::CloudWatch::Client.new(region: 'us-west-2')
 end
 
+def put_cloudwatch_data(namespace, name_metric, dimension_name, dimension_value, datapoint_value)
+# make PutMetricData api call to cloudwatch using custom metrics
+  @cloudwatch.put_metric_data({
+    namespace: namespace,
+    metric_data: [
+      {
+        metric_name: name_metric,
+        dimensions: [
+          {
+            name: dimension_name,
+            value: dimension_value,
+          },
+        ],
+        timestamp: Time.now,
+        value: datapoint_value,
+        unit: "Count",
+        storage_resolution: 1,
+      },
+    ],
+  })
+end
+
+# SNS
 def sns_client_constructor
   @sns = Aws::SNS::Client.new(
     region: 'us-west-2'
